3d00616d202aad2d83d4fe2005a3b9e3012a705c
yesmine-code <65494405+yesmine-code@users.noreply.github.com>
2020-05-21 18:59:29 +0200

    fix return

--- libft_project/libft/ft_strtrim.c
+++ libft_project/libft/ft_strtrim.c
@@ -20,7 +20,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (!s1 || !set)
 		return (NULL);
 	if (*s1 == '\0')
-		return (NULL);
+		return ((char *)s1);
 	while (*s1 && ft_strchr(set, *s1))
 		s1++;
 	size = ft_strlen(s1);
