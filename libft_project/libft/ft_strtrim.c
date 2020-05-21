cd6a2a9523dc42a47cb66147fe71836d8f325273
yesmine-code <65494405+yesmine-code@users.noreply.github.com>
2020-05-21 18:49:21 +0200

    fix retrun if *s1 == '\0'

--- libft_project/libft/ft_strtrim.c
+++ libft_project/libft/ft_strtrim.c
@@ -20,7 +20,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (!s1 || !set)
 		return (NULL);
 	if (*s1 == '\0')
-		return ((char *)s1);
+		return (NULL);
 	while (*s1 && ft_strchr(set, *s1))
 		s1++;
 	size = ft_strlen(s1);
