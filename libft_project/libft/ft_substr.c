0d91922187d5cfd79d8d652639542959f2ed0abd
yesmine-code <65494405+yesmine-code@users.noreply.github.com>
2020-05-22 20:01:58 +0200

    fix condition

--- libft_project/libft/ft_substr.c
+++ libft_project/libft/ft_substr.c
@@ -19,7 +19,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (s == NULL)
 		return (NULL);
-	if (ft_strlen(s) < start)
+	if (ft_strlen(s) <= start)
 		return(ft_strdup(""));
 	i = 0;
 	ptr = malloc(sizeof(*ptr) * (len + 1));
