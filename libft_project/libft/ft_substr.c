87339eb033a23777b67f95061b9eadd737417345
yesmine-code <65494405+yesmine-code@users.noreply.github.com>
2020-05-24 15:04:42 +0200

    fix norme

--- libft_project/libft/ft_substr.c
+++ libft_project/libft/ft_substr.c
@@ -20,7 +20,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (s == NULL)
 		return (NULL);
 	if (ft_strlen(s) < start)
-		return(ft_strdup(""));
+		return (ft_strdup(""));
 	i = 0;
 	ptr = malloc(sizeof(*ptr) * (len + 1));
 	if (ptr == NULL)
