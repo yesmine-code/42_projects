cfaf02a184036bde8dcf66ef690f7a6e37c789d9
Yesmine BESBES <ybesbes@e3r7p10.42.fr>
2020-06-02 12:22:56 +0200

    get_next_line files

---
+++ libft_project/libft/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybesbes <ybesbes@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/02 12:10:13 by ybesbes           #+#    #+#             */
+/*   Updated: 2020/06/02 12:19:24 by ybesbes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
+
+int	get_next_line(int fd, char **line);
+
+#endif
