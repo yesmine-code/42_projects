b1a8fbdd4d2840bbf3bbee35715ca37f0b6db75f
yesmine <yesminerekik5@gmail.com>
2020-06-04 00:41:12 +0200

    gnl

--- libft_project/libft/get_next_line.c
+++ libft_project/libft/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ybesbes <ybesbes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/02 12:07:08 by ybesbes           #+#    #+#             */
-/*   Updated: 2020/06/02 18:43:54 by ybesbes          ###   ########.fr       */
+/*   Updated: 2020/06/04 00:40:09 by ybesbes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -16,70 +16,93 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <fcntl.h>
+int	get_matrix_length(char **ptr)
+{
+	int	i;
+
+	i = 0;
+	while (ptr[i])
+		i++;
+	return (i);
+}
+
+void	to_free(char **ptr)
+{
+	char	**tmp;
+
+	tmp = ptr;
+	while (*tmp)
+	{ 
+		free(tmp++);
+	}
+	//free(ptr);
+}
 
 char	*ft_realloc(char *ptr, size_t size)
 {
-	char	*ptr2;
+	void	*ptr2;
 
-	ptr2= malloc( sizeof(ptr) + size);
-	ft_strlcpy(ptr2, ptr, ft_strlen(ptr) + 1);
-	free(ptr);
-	return (ptr2);
+	if (ptr == NULL)
+	{
+		ptr = ft_calloc(size, 1);
+		return ptr;
+	}
+	else
+	{
+		ptr2 = ft_calloc(sizeof(ptr) + size, 1);
+		if (ptr2)
+			ft_strlcpy(ptr2, ptr, ft_strlen(ptr) + 1);
+		free(ptr);
+		return ptr2;
+	}
 }
+
 int	get_next_line(int fd, char **line)
 {
-	int BUFFER_SIZE = 4;
 	static char	*buf;
 	char		*tmp;
+	int		line_length;
+	int		bytes_read;
 
-	if (fd < 0 || line == NULL)
+	if (fd < 0)
 		return (-1);
-//	buf = NULL;
-	while (1)
+	while (buf == NULL || (!(ft_strchr(buf, '\n'))))
 	{
-		if (buf == NULL)
+		buf = ft_realloc(buf, BUFFER_SIZE);
+		if(buf == NULL)
 		{
-			printf("1- buffer est null\n");
-			buf = malloc(BUFFER_SIZE);
-			read(fd, buf, BUFFER_SIZE);
+			to_free(line);
+			return (-1);
 		}
-		else if (buf != NULL && ft_strchr(buf, '\n') == NULL)
-		{
-			printf("2- pas de retour\n");
-			printf("\n%s\n", buf);
-			buf = ft_realloc(buf, BUFFER_SIZE);
-			printf("sizeof devient %lu, buf devien %s\n", sizeof(buf), buf);
-			read(fd, buf + ft_strlen(buf), BUFFER_SIZE);
-			printf("sizeof devient %lu, buf devien %s\n", sizeof(buf), buf);
-		}
-		else
-		{
-			printf("3- buffer est");
-			printf("\n%s\n", buf);
-			int s = ft_strchr(buf, '\n') - buf;
-			*line = ft_substr(buf, 0, s);
-			printf("\nline:\n%s\n", *line);
-			tmp = ft_strdup(ft_strchr(buf, '\n') + 1);
-			free(buf);
-			buf = tmp;
-			printf("buffer devient: \n%s" , buf);
-			break;
-		}
+		bytes_read = read(fd, buf + ft_strlen(buf), BUFFER_SIZE);
+		if (bytes_read == 0) //eof
+			return (0);
 
 	}
-	return (0);
+	line_length = ft_strchr(buf, '\n') - buf;
+	line[get_matrix_length(line)] = ft_substr(buf, 0, line_length);
+	tmp = ft_strdup(ft_strchr(buf, '\n') + 1);
+	free(buf);
+	buf = tmp;
+	return (1);
 }
 
 int main(void)
 {
 	char	**ptr;
-
-	ptr = malloc(21);
+	int	i;
+	
+	i = 0;
+	ptr = NULL;
+	ptr = malloc(100);
 	int fd = open("text.txt", O_RDONLY);
-	get_next_line(fd, ptr);
-	get_next_line(fd, ptr + 1);
-	get_next_line(fd, ptr + 2);
-	get_next_line(fd, ptr + 3);
+	printf("starting test \n");
+	while (get_next_line(fd, ptr) == 1)
+	{
+		printf("---\n%d: %s\n",i,ptr[i]);
+		i++;
+	}
+	printf("end of the test");
 	close(fd);
 	return (0);
 }
