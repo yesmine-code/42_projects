e9ec384f483d1c18570d6e11e5c4ddd7abe6f936
Yesmine BESBES <ybesbes@e3r11p1.42.fr>
2020-06-04 19:45:25 +0200

    gnl clean

--- libft_project/libft/get_next_line.c
+++ libft_project/libft/get_next_line.c
@@ -6,9 +6,10 @@
 /*   By: ybesbes <ybesbes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/02 12:07:08 by ybesbes           #+#    #+#             */
-/*   Updated: 2020/06/04 13:06:02 by ybesbes          ###   ########.fr       */
+/*   Updated: 2020/06/04 19:41:35 by ybesbes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 #include "get_next_line.h"
 #include <unistd.h>
@@ -16,7 +17,8 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <fcntl.h>
-int	get_matrix_length(char **ptr)
+
+int		get_matrix_length(char **ptr)
 {
 	int	i;
 
@@ -26,62 +28,55 @@ int	get_matrix_length(char **ptr)
 	return (i);
 }
 
-void	to_free(char **ptr)
+int		to_free(char *ptr)
 {
-	char	**tmp;
-
-	tmp = ptr;
-	while (*tmp)
-	{ 
-		free(tmp++);
-	}
-	//free(ptr);
+	free(ptr);
+	return (-1);
 }
 
 char	*ft_realloc(char *ptr, size_t size)
 {
 	char	*ptr2;
+	int		final_size;
+	int		i;
 
+	i = 0;
 	if (ptr == NULL)
-	{
-		ptr = ft_calloc(size, 1);
-		return ptr;
-	}
+		final_size = size;
 	else
+		final_size = size + sizeof(ptr);
+	ptr2 = malloc(final_size);
+	if (ptr2 != NULL)
 	{
-		ptr2 = ft_calloc(sizeof(ptr) + size, 1);
-		if (ptr2)
+		while (i < final_size)
+			ptr2[i++] = 0;
+		if (ptr)
 			ft_strlcpy(ptr2, ptr, ft_strlen(ptr) + 1);
-		free(ptr);
-		return ptr2;
 	}
+	if (ptr)
+		free(ptr);
+	return (ptr2);
 }
 
-int	get_next_line(int fd, char **line)
+int		get_next_line(int fd, char **line)
 {
 	static char	*buf;
 	char		*tmp;
-	int		line_length;
-	int		bytes_read;
+	int			bytes_read;
+	int			line_length;
 
 	if (fd < 0)
 		return (-1);
 	while (buf == NULL || (!(ft_strchr(buf, '\n'))))
 	{
 		buf = ft_realloc(buf, BUFFER_SIZE);
-		if(buf == NULL)
-		{
-			free(buf);
-			return (-1);
-		}
+		if (buf == NULL)
+			return (to_free(buf));
 		bytes_read = read(fd, buf + ft_strlen(buf), BUFFER_SIZE);
-		if (bytes_read == 0) //eof
+		if (bytes_read == 0)
 			return (0);
 		else if (bytes_read == -1)
-		{
-			free(buf);
-			return (-1);
-		}
+			return (to_free(buf));
 	}
 	line_length = ft_strchr(buf, '\n') - buf;
 	line[get_matrix_length(line)] = ft_substr(buf, 0, line_length);
@@ -91,22 +86,12 @@ int	get_next_line(int fd, char **line)
 	return (1);
 }
 
-int main(void)
+int main()
 {
-	char	**ptr;
-	int	i;
-
-	i = 0;
-	ptr = NULL;
-	ptr = malloc(100);
+	char *ptr[255];
+	int i = 0;
 	int fd = open("text.txt", O_RDONLY);
-	printf("starting test \n");
 	while (get_next_line(fd, ptr) == 1)
-	{
-		printf("---\n%d: %s\n",i,ptr[i]);
-		i++;
-	}
-	printf("end of the test");
+		printf("%s\n", ptr[i++]);
 	close(fd);
-	return (0);
 }
--- libft_project/libft/get_next_line_utils.c
+++ libft_project/libft/get_next_line_utils.c
@@ -6,8 +6,106 @@
 /*   By: ybesbes <ybesbes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/02 12:12:16 by ybesbes           #+#    #+#             */
-/*   Updated: 2020/06/02 12:12:50 by ybesbes          ###   ########.fr       */
+/*   Updated: 2020/06/04 19:44:07 by ybesbes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	const char	*ptr;
+	size_t		i;
+
+	ptr = str;
+	i = 0;
+	while (ptr[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*dest;
+	int		i;
+
+	dest = malloc(sizeof(*dest) * (ft_strlen(s) + 1));
+	i = 0;
+	if (dest != NULL)
+	{
+		while (s[i] != '\0')
+		{
+			dest[i] = s[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (dest);
+}
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (src == NULL)
+	{
+		dest = NULL;
+		return (0);
+	}
+	while (i < size && *src != '\0')
+	{
+		*dest++ = *src++;
+		i++;
+	}
+	if (i < size)
+	{
+		*dest = '\0';
+		return (i);
+	}
+	else if (size > 0)
+	{
+		*(dest - 1) = '\0';
+		return (i + ft_strlen(src));
+	}
+	else
+		return (ft_strlen(src));
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if ((char)c == '\0')
+		return ((char *)s + ft_strlen(s));
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return ((char*)s + i);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ptr;
+	size_t	i;
+
+	if (s == NULL)
+		return (NULL);
+	if (ft_strlen(s) < start)
+		return (ft_strdup(""));
+	i = 0;
+	ptr = malloc(sizeof(*ptr) * (len + 1));
+	if (ptr == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		ptr[i] = s[start + i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
